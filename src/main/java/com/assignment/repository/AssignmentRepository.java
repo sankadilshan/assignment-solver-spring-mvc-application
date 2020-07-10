67fcd865de05a6c3b8e3a2baa673f4d8ca7b0cb0
sanka9686 <sankadilshan9686@gmail.com>
2020-07-10 18:59:02 +0530

    changes

--- src/main/java/com/assignment/repository/AssignmentRepository.java
+++ src/main/java/com/assignment/repository/AssignmentRepository.java
@@ -2,7 +2,6 @@ package com.assignment.repository;
 
 import com.assignment.model.Assignment;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
